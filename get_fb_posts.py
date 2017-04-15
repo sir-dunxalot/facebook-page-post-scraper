6e21ef00adbecbe12b16dba8a809a1daa32e6c44
Duncan Graham Walker <walkerdu@bc.edu>
2017-04-14 21:16:56 -0400

    Ability to scrape multiple pages with one command

---
+++ get_fb_posts.py
@@ -0,0 +1,260 @@
+import urllib2
+import json
+import datetime
+import csv
+import time
+
+# Start of options
+
+app_id = "1448270435248078"
+app_secret = "02cc640f23fbe7824b8aa8678178c56e" # DO NOT SHARE WITH ANYONE!
+page_id = "21317493981"
+date_since = "01-03-2017" # %d-%m-%Y or None
+date_until = "31-03-2017" # %d-%m-%Y or None
+get_reaction_stats = False;
+output_dir = 'scraped_data'
+
+pages = [
+  {
+    'page_id': '146505212039213',
+    'name': 'Unilad',
+    'category': 'Hype'
+  }, {
+    'page_id': '199098633470668',
+    'name': 'LAD Bible',
+    'category': 'Hype'
+  }
+]
+
+# Unilad  Hype    146505212039213
+# Lab Bible   Hype    199098633470668
+# Barstool sports Hype    348156187501
+# Buzzfeed    Hype    21898300328
+# Huffington post News    18468761129
+# Circa   News    442430012490396
+# Washington Post News    6250307292
+# BBC News    228735667216
+# ESPN    Sports  104266592953439
+# Goal.com    Sports  25427813597
+# Vogue   Health & beauty 42933792278
+# Health.com  Health & beauty 14482400667
+# Women's Health  Health & beauty 34834516787
+# Men's fitness   Health & beauty 14866608226
+# Glamour Health & beauty 26815555478
+# Food Network    Food    20534666726
+# Taste of Home   Food    61511025427
+# CondeNastTraveler   Travel  21317493981
+# Expedia Travel  64067037679
+# HBO Entertainment   113408673932
+# Warner Bros Entertainment   296025760408983
+# Rolling Stone   Entertainment   10664530778
+
+# End of options
+
+access_token = app_id + "|" + app_secret
+
+def request_until_succeed(url):
+  req = urllib2.Request(url)
+  success = False
+
+  while success is False:
+    try:
+      response = urllib2.urlopen(req)
+      if response.getcode() == 200:
+          success = True
+    except Exception, e:
+      print e
+      time.sleep(5)
+
+      print "Error for URL %s: %s" % (url, datetime.datetime.now())
+      print "Retrying."
+
+  return response.read()
+
+# Needed to write tricky unicode correctly to csv
+def unicode_normalize(text):
+  return text.translate({ 0x2018:0x27, 0x2019:0x27, 0x201C:0x22, 0x201D:0x22,
+                            0xa0:0x20 }).encode('utf-8')
+
+def getFacebookPageFeedData(page_id, access_token, num_statuses):
+
+  # Construct the URL string; see http://stackoverflow.com/a/37239851 for
+  # Reactions parameters
+  base = "https://graph.facebook.com/v2.6"
+  node = "/%s/posts" % page_id
+  fields = "/?fields=message,link,created_time,type,name,id,properties," + \
+          "comments.limit(0).summary(true),shares,reactions" + \
+          ".limit(0).summary(true)"
+  parameters = "&limit=%s&access_token=%s" % (num_statuses, access_token)
+  url = base + node + fields + parameters
+
+  # If a date range is provided, use it
+
+  if date_since is not None:
+    timestamp_since = time.mktime(datetime.datetime.strptime(date_since, "%d-%m-%Y").timetuple())
+    url = url + "&since=%s" % timestamp_since
+
+  if date_until is not None:
+    timestamp_until = time.mktime(datetime.datetime.strptime(date_until, "%d-%m-%Y").timetuple())
+    url = url + "&until=%s" % timestamp_until
+
+  # retrieve data
+  data = json.loads(request_until_succeed(url))
+
+  return data
+
+def getReactionsForStatus(status_id, access_token):
+
+  # See http://stackoverflow.com/a/37239851 for Reactions parameters
+      # Reactions are only accessable at a single-post endpoint
+
+  base = "https://graph.facebook.com/v2.6"
+  node = "/%s" % status_id
+  reactions = "/?fields=" \
+          "reactions.type(LIKE).limit(0).summary(total_count).as(like)" \
+          ",reactions.type(LOVE).limit(0).summary(total_count).as(love)" \
+          ",reactions.type(WOW).limit(0).summary(total_count).as(wow)" \
+          ",reactions.type(HAHA).limit(0).summary(total_count).as(haha)" \
+          ",reactions.type(SAD).limit(0).summary(total_count).as(sad)" \
+          ",reactions.type(ANGRY).limit(0).summary(total_count).as(angry)"
+  parameters = "&access_token=%s" % access_token
+  url = base + node + reactions + parameters
+
+  # retrieve data
+  data = json.loads(request_until_succeed(url))
+
+  return data
+
+
+def processFacebookPageFeedStatus(status, access_token):
+
+  # The status is now a Python dictionary, so for top-level items,
+  # we can simply call the key.
+
+  # Additionally, some items may not always exist,
+  # so must check for existence first
+
+  status_id = status['id']
+  status_message = '' if 'message' not in status.keys() else \
+          unicode_normalize(status['message'])
+  link_name = '' if 'name' not in status.keys() else \
+          unicode_normalize(status['name'])
+  status_type = status['type']
+  status_link = '' if 'link' not in status.keys() else \
+          unicode_normalize(status['link'])
+
+  # Get length, if available
+
+  status_properties = [] if 'properties' not in status.keys() else \
+          status['properties']
+  status_length_properties = filter(lambda x: x['name'] == 'Length', status_properties) # Find length
+  status_video_length = '' if len(status_length_properties) == 0 else \
+          status_length_properties[0]['text']
+
+  # Time needs special care since a) it's in UTC and
+  # b) it's not easy to use in statistical programs.
+
+  status_published = datetime.datetime.strptime(
+          status['created_time'],'%Y-%m-%dT%H:%M:%S+0000')
+  status_published = status_published + \
+          datetime.timedelta(hours=-5) # EST
+  status_published = status_published.strftime(
+          '%Y-%m-%d %H:%M:%S') # best time format for spreadsheet programs
+
+  # Nested items require chaining dictionary keys.
+
+  num_reactions = 0 if 'reactions' not in status else \
+          status['reactions']['summary']['total_count']
+  num_comments = 0 if 'comments' not in status else \
+          status['comments']['summary']['total_count']
+  num_shares = 0 if 'shares' not in status else status['shares']['count']
+
+  # Counts of each reaction separately; good for sentiment
+  # Only check for reactions if past date of implementation:
+  # http://newsroom.fb.com/news/2016/02/reactions-now-available-globally/
+
+  reactions = getReactionsForStatus(status_id, access_token) if \
+          status_published > '2016-02-24 00:00:00' and get_reaction_stats else {}
+
+  num_likes = 0 if 'like' not in reactions else \
+          reactions['like']['summary']['total_count']
+
+  # Special case: Set number of Likes to Number of reactions for pre-reaction
+  # statuses
+
+  num_likes = num_reactions if status_published < '2016-02-24 00:00:00' \
+          else num_likes
+
+  def get_num_total_reactions(reaction_type, reactions):
+    if reaction_type not in reactions:
+      return 0
+    else:
+      return reactions[reaction_type]['summary']['total_count']
+
+  num_loves = get_num_total_reactions('love', reactions)
+  num_wows = get_num_total_reactions('wow', reactions)
+  num_hahas = get_num_total_reactions('haha', reactions)
+  num_sads = get_num_total_reactions('sad', reactions)
+  num_angrys = get_num_total_reactions('angry', reactions)
+
+  # Return a tuple of all processed data
+
+  return (status_id, status_message, link_name, status_type, status_link,
+          status_published, status_video_length, num_reactions, num_comments, num_shares,
+          num_likes, num_loves, num_wows, num_hahas, num_sads, num_angrys)
+
+def scrapeFacebookPageFeedStatus(page, access_token):
+  page_id = page['page_id']
+  page_name = page['name']
+
+  # Append date range to filename
+
+  csv_filename = '%s/%s_facebook_statuses_%s_%s.csv' % (output_dir, page_id, date_since, date_until)
+
+  with open(csv_filename, 'wb') as file:
+    w = csv.writer(file)
+    w.writerow(["status_id", "status_message", "link_name", "status_type",
+                "status_link", "status_published", "status_video_length", "num_reactions",
+                "num_comments", "num_shares", "num_likes", "num_loves",
+                "num_wows", "num_hahas", "num_sads", "num_angrys"])
+
+    has_next_page = True
+    num_processed = 0   # keep a count on how many we've processed
+    scrape_starttime = datetime.datetime.now()
+
+    print "Scraping Page: %s (%s)" % (page_name, page_id)
+
+    statuses = getFacebookPageFeedData(page_id, access_token, 100)
+
+    while has_next_page:
+      for status in statuses['data']:
+
+        # Ensure it is a status with the expected metadata
+        if 'reactions' in status:
+          w.writerow(processFacebookPageFeedStatus(status,
+                access_token))
+
+        # output progress occasionally to make sure code is not
+        # stalling
+        num_processed += 1
+
+        if num_processed % 100 == 0:
+          print "%s Statuses Processed: %s" % \
+                (num_processed, datetime.datetime.now())
+
+      # if there is no next page, we're done.
+      if 'paging' in statuses.keys():
+        statuses = json.loads(request_until_succeed(
+                                  statuses['paging']['next']))
+      else:
+        has_next_page = False
+
+    file.close()
+
+    print "\nDone!\n%s Statuses Processed in %s\n" % \
+            (num_processed, datetime.datetime.now() - scrape_starttime)
+
+
+if __name__ == '__main__':
+  for page in pages:
+    scrapeFacebookPageFeedStatus(page, access_token)
